4db6bcc58d2ced73a0d68554035104936a82c5fb
LudaTverday <Ludachka22@gmail.com>
2022-10-20 13:30:30 +0300

    HW#9 hide-add properties of element

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ console.log("Launching scrips");
 const anchors = document.querySelectorAll (".thambnails-ancor");
 const detailsImage = document.querySelector (".image-title");
 const detailsTitle = document.querySelector (".details-title");
+const mainClass = document.querySelector (".main-class");
+const HIDDEN = "hidden";
 function setDetails (anchor){
     detailsImage.setAttribute ('src', anchor.getAttribute('data-details-image'));
     detailsTitle.innerHTML = anchor.getAttribute ('data-details-title');
@@ -9,6 +11,13 @@ function setDetails (anchor){
 for(let i=0; i<anchors.length; i++){
     anchors[i].addEventListener ("click", function (){
         console.log ("event-click on", anchors [i]);
+        showDetails ();
         setDetails (anchors[i]);
     })
+}
+function showDetails (){
+mainClass.classList.remove ("hidden");
+}
+function hideDetails () {
+mainClass.classList.add (HIDDEN);
 }
