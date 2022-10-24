f366d1d49f240e7d569bf8ecd19ec160531a1942
LudaTverday <Ludachka22@gmail.com>
2022-10-25 00:55:13 +0300

    HW#10 adding audio for each item

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ const anchors = document.querySelectorAll (".thambnails-ancor");
 const detailsImage = document.querySelector (".image-title");
 const detailsTitle = document.querySelector (".details-title");
 const mainClass = document.querySelector (".main-class");
+const detailContainer = document.querySelector (".detail-container");
+const detailAudio = document.querySelector (".detail-audio");
 const HIDDEN = "hidden";
+const IS_POINT = "is-point";
 function setDetails (anchor){
     detailsImage.setAttribute ('src', anchor.getAttribute('data-details-image'));
     detailsTitle.innerHTML = anchor.getAttribute ('data-details-title');
+    detailAudio.setAttribute ('src', anchor.getAttribute('data-details-audio'));
 }
 for(let i=0; i<anchors.length; i++){
     anchors[i].addEventListener ("click", function (){
@@ -17,6 +21,14 @@ for(let i=0; i<anchors.length; i++){
 }
 function showDetails (){
 mainClass.classList.remove ("hidden");
+detailContainer.classList.add (IS_POINT);
+setTimeout(function(){
+    detailContainer.classList.remove (IS_POINT)
+    detailAudio.play();
+},1);
+setTimeout (function() {
+    detailAudio.pause ();
+},5000);
 }
 function hideDetails () {
 mainClass.classList.add (HIDDEN);
