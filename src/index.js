0a626850f41f25f070fda7dbfba731f89365ea92
LudaTverday <Ludachka22@gmail.com>
2022-10-29 15:18:25 +0300

    LovBeaStore finale solution

--- src/index.js
+++ src/index.js
@@ -11,25 +11,28 @@ function setDetails (anchor){
     detailsImage.setAttribute ('src', anchor.getAttribute('data-details-image'));
     detailsTitle.innerHTML = anchor.getAttribute ('data-details-title');
     detailAudio.setAttribute ('src', anchor.getAttribute('data-details-audio'));
+    
 }
 for(let i=0; i<anchors.length; i++){
     anchors[i].addEventListener ("click", function (){
         console.log ("event-click on", anchors [i]);
-        showDetails ();
         setDetails (anchors[i]);
+        showDetails ();        
     })
 }
 function showDetails (){
 mainClass.classList.remove ("hidden");
 detailContainer.classList.add (IS_POINT);
 setTimeout(function(){
-    detailContainer.classList.remove (IS_POINT)
-    detailAudio.play();
+    detailContainer.classList.remove (IS_POINT);   
 },1);
-setTimeout (function() {
+detailAudio.play ();
+setTimeout (function () {
     detailAudio.pause ();
-},5000);
+   /* detailAudio.currentTime=0;*/
+ },5000);
 }
-function hideDetails () {
+function hideDetails () { 
 mainClass.classList.add (HIDDEN);
+detailAudio.pause ();
 }
