b093e328be1420ebbfe7e7c8025a2a2feb68a560
LudaTverday <Ludachka22@gmail.com>
2022-11-27 14:13:17 +0200

    HW#19 corrected remarks

--- employeesRandom.js
+++ employeesRandom.js
@@ -1,21 +1,22 @@
 //task 1 - createRandomEmployees
-const arrayId = [];
 
 function createRandomEmployees(nEmployees, idDigits, minSalary, maxSalary, minBirthYear, maxBirthYear) {
     const arrRandomEmployees = [];
+    const arrayId = [];
+    let idMin = 10 ** (idDigits - 1);
+    let idMax = 10 ** (idDigits);
     while (nEmployees) {
         nEmployees--;
-        let id = idRandom(idDigits);
+        let id = idRandom(idMin, idMax, arrayId);
         arrRandomEmployees[nEmployees] = createEmployee(id, "name" + id,
             getRandomValue(minBirthYear, maxBirthYear), getRandomValue(minSalary, maxSalary));
     }
     return arrRandomEmployees;
 }
-function idRandom(idDigits) {
-    let idMin = 10 ** (idDigits - 1);
-    let idMax = 10 ** (idDigits);
+
+function idRandom(idMin, idMax, arrayId) {
     let id = getRandomValue(idMin, idMax);
-    return checkId_IsUnick(id, idMin, idMax);
+    return checkId_IsUnick(id, idMin, idMax, arrayId);
 }
 function getRandomValue(minValue, maxValue) {
     return maxValue - Math.trunc(Math.random() * (maxValue - minValue));
@@ -23,7 +24,7 @@ function getRandomValue(minValue, maxValue) {
 function createEmployee(id, name, birthYear, salary) {
     return { id, name, birthYear, salary };
 }
-function checkId_IsUnick(id, idMin, idMax) {
+function checkId_IsUnick(id, idMin, idMax, arrayId) {
     while (arrayId[id] == id) {
         id = getRandomValue(idMin, idMax);
     }
@@ -31,7 +32,7 @@ function checkId_IsUnick(id, idMin, idMax) {
     return id;
 }
 
-const arrEmployees = createRandomEmployees(10, 3, 10000, 20000, 1950, 2006);
+const arrEmployees = createRandomEmployees(7, 3, 10000, 20000, 1950, 2006);
 console.log(`Employees of company XXX:`);
 console.log(arrEmployees);
 
@@ -39,18 +40,18 @@ console.log('**********************************************');
 //task 2 Average Age all Employees
 
 function getAverageAge(arrEmployees) {
-    let ageAverage = arrEmployees.reduce((ageAverage, employee) => {
+    let ageSum = arrEmployees.reduce((ageSum, employee) => {
         let age = getAgeEmployee(employee);
-        ageAverage += (age / arrEmployees.length);
-        return ageAverage;
+        ageSum += age;
+        return ageSum;
     }, 0)
-    return Math.floor(ageAverage);
+    return Math.floor(ageSum / arrEmployees.length);
 }
 function getAgeEmployee(employee) {
     return new Date().getFullYear() - employee.birthYear;
 }
 
-console.log (`Average age of employees: ${getAverageAge(arrEmployees)}`);
+console.log(`Average age of employees: ${getAverageAge(arrEmployees)}`);
 
 console.log('**********************************************');
 //task 3 sort salary from/to
@@ -58,7 +59,7 @@ console.log('**********************************************');
 function getEmployeesBySalary(arrEmployees, salaryFrom, salaryTo) {
     let arSalaryFromTo = arrEmployees.filter(employee =>
         employee.salary >= salaryFrom && employee.salary <= salaryTo);
-    return arSalaryFromTo.sort((empl1, empl2) => empl1.salary > empl2.salary ? 1 : -1);
+    return arSalaryFromTo.sort((empl1, empl2) => empl1.salary - empl2.salary);
 }
 
 let salaryFrom = 13000;
