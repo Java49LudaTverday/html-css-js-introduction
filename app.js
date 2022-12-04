73dcbeba471d3cf7b14060188c31c2a489627992
LudaTverday <Ludachka22@gmail.com>
2022-12-04 20:40:30 +0200

    CW#22 Object Oriented Introduction, Rectangle

---
+++ app.js
@@ -0,0 +1,58 @@
+/*function f1() {
+    const a = this;
+    return a;
+}
+const f2 = () => {
+    const a = this;
+    return a;
+}
+const f = f1;
+f();
+f2();
+f1.d = 20;
+console.log(f1.d);
+console.log(f1); //[Function: f1] { d: 20 }
+console.log(f); //[Function: f1] { d: 20 }
+console.log(f1.this);//undefined*/
+/*const rectangle = {
+    width: 3,
+    height: 5,
+    square: function() {
+        return this.width * this.height;
+    },
+    perimeter: () => {
+        return this.width*2 + this.height*2; //NaN
+    }
+}
+//console.log(rectangle.square());
+//console.log(rectangle.perimeter());
+
+const rectangle1 = {
+    width: 3,
+    height: 5,
+    square: function() {
+        return this.width * this.height;
+    },
+    perimeter: function () {
+        return this.width*2 + this.height*2;
+    },
+    display: function() {
+        console.log(`square is ${this.square()}; perimeter is ${this.perimeter()}`);
+    }
+}*/
+function Rectangle (width, height){
+    this.width = width;
+    this.height = height;
+}
+Rectangle.prototype.square = function() {
+    return this.width * this.height;
+}
+Rectangle.prototype.perimeter = function() {
+    return this.width*2 + this.height*2;
+}
+const rectangle = new Rectangle(3, 5);
+
+console.log(rectangle.square());
+console.log(rectangle.perimeter());
+
+//rectangle1.display();
