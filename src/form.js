df53d907bc084a10171b2ed4d87a278421ceaabf
LudaTverday <Ludachka22@gmail.com>
2022-12-06 12:56:05 +0200

    HW#22 AddAndPresentingEmployees

--- src/form.js
+++ src/form.js
@@ -1,7 +1,25 @@
 const inputElements = document.querySelectorAll(".form-class [name]");
+const informPlace = document.querySelector("#place-form");
+const OlAllEmployees = document.querySelector(".allEmployees");
+const emplForm = document.getElementById("employee-form");
+const sectEmpl = document.querySelector(".section-allEmployees");
+const OLemplBySalary = document.querySelector(".emplBySalary");
+const sectFilterSalary = document.querySelector(".filterSalary");
+const informParag = document.querySelector(".inform-p");
+
 const YEAR_MIN = 1959;
 const YEAR_MAX = new Date().getFullYear();
-const ACode = "A".charCodeAt();
+const aCode = "a".charCodeAt();
+const zCode = "z".charCodeAt();
+const MIN_SALARY = 1000;
+const MAX_SALARY = 40000;
+const TIME_OF_OUT = 5000;
+let FROM_SALARY = MIN_SALARY;
+let TO_SALARY = MAX_SALARY;
+const RED = "red";
+const BLACK = "black";
+
+const company = new Company();
 function onSubmit(event) {
     event.preventDefault();
     console.log("submitted");
@@ -10,31 +28,49 @@ function onSubmit(event) {
         return res;
     }, {});
     console.log(employee);
+    company.hireEmployee(employee);
 }
 function onChange(event) {
     let nameElement = event.target.name;
+    let elementEvent = event.target;
     switch (nameElement) {
-        case "employee-name":
-            if (checkisValidName(event) == false) {
+        case "employee_name":
+            console.log(elementEvent.value)
+            if (!checkisValidName(elementEvent)) {
                 errorEvent(event, nameElement);
             }
             break;
         case "email":
-            if (checkisValidEmail(event) == false) {
+            console.log(elementEvent.value)
+            if (!checkisValidEmail(elementEvent)) {
                 errorEvent(event, nameElement);
             }
             break;
         case "birthDate":
-            if (checkisValidBirthDate(event) == false) {
+            if (!checkisValidBirthDate(elementEvent)) {
                 errorEvent(event, nameElement);
             }
             break;
         case "salary":
-            if (event.target.value < 1000 || event.target.value > 40000) {
+            if (!checkisValidSalary(elementEvent)) {
                 errorEvent(event, nameElement);
             }
             break;
-    }   
+        case "salaryFrom":
+            FROM_SALARY = +elementEvent.value;
+            if (checkisValidFromToSalary(FROM_SALARY, TO_SALARY)) {
+                outputEmplSalaryFromTo(FROM_SALARY, TO_SALARY);
+                informParag.innerHTML = `List of employees with salary from ${FROM_SALARY} to ${TO_SALARY}:`;
+            }
+            break;
+        case "salaryTo":
+            TO_SALARY = +elementEvent.value;
+            if (checkisValidFromToSalary(FROM_SALARY, TO_SALARY)) {
+                outputEmplSalaryFromTo(FROM_SALARY, TO_SALARY);
+                informParag.innerHTML = `List of employees with salary from ${FROM_SALARY} to ${TO_SALARY}:`;
+            }
+            break
+    }
 }
 function errorEvent(event, nameElement) {
     event.target.value = '';
@@ -45,27 +81,16 @@ function errorEvent(event, nameElement) {
 function timeoutError(nameElement) {
     setTimeout(() => {
         document.getElementById(`error_${nameElement}`).innerHTML = ``;
-    }, 3000)
+    }, TIME_OF_OUT)
 }
-function checkisValidName(event) {
-   let res = true;
-    let arrayInput = Array.from(event.target.value);
-   /*let res = arrayInput.every(letter => {
-        let charCodeLet = letter.charCodeAt(0);
-        (isNaN(+letter)|| (charCodeLet >=ACode && charCodeLet< ACode+26) || (charCodeLet>(ACode+33)&&charCodeLet<=("z".charCodeAt()))) 
-    })*/
-   arrayInput.forEach(elem => {
-        let number = +elem;
-        if (!isNaN(number)) {
-            res = false;
-        }
-    })
-    return res;
+// Validation functions:
+function checkisValidName(elementEvent) {
+    let arrayName = Array.from(elementEvent.value.toLowerCase());
+    return arrayName.every(elem => elem.charCodeAt(0) >= aCode && elem.charCodeAt(0) <= zCode);
 }
-
-function checkisValidEmail(event) {
+function checkisValidEmail(elementEvent) {
     let res = false;
-    let arrayInput = Array.from(event.target.value);
+    let arrayInput = Array.from(elementEvent.value);
     arrayInput.forEach(elem => {
         let at = "@";
         if (elem === at) {
@@ -74,29 +99,86 @@ function checkisValidEmail(event) {
     })
     return res;
 }
-function checkisValidBirthDate(event) {
+function checkisValidBirthDate(elementEvent) {
     let res = true;
-    let arrayInput = Array.from(event.target.value);
-    let year = arrayInput.slice(0, 4).join('');
-    console.log(year);
+    let year = elementEvent.value.slice(0, 4);
     if (year < YEAR_MIN || year > YEAR_MAX) {
         res = false;
     }
     return res;
 }
+function checkisValidSalary(elementEvent) {
+    let res = true;
+    if (elementEvent.value < MIN_SALARY || elementEvent.value > MAX_SALARY) {
+        res = false;
+    }
+    return res;
+}
+function checkisValidFromToSalary(salaryFrom, salaryTo) {
+    let res = true;
+    if (salaryTo < salaryFrom) {
+        informParag.style.color = RED;
+        informParag.innerHTML = `*salary "from" must be less salary "to"`;  //`Please enter salary from ${MIN_SALARY} to ${MAX_SALARY} include`;
+        res = false;
+    } else if (salaryFrom > MAX_SALARY || salaryTo > MAX_SALARY) {
+        informParag.style.color = RED;
+        informParag.innerHTML = `*salary must be less ${MAX_SALARY} (included)`;
+        res = false;
+    }
+    return res;
+}
 
- /* if (nameElement == "employee-name") {
-         if (checkisValidName(event) == false) {
-             errorEvent (event, nameElement);
-         }
-     }
-     if (nameElement == "email") {
-         if (checkisValidEmail(event) == false) {
-             errorEvent(event, nameElement);
-         }
-     }
-     else if (nameElement == "salary") {
-         if (event.target.value < 1000 || event.target.value > 40000) {
-             errorEvent(event, nameElement);
-         }
-     }*/
+// object Company ***********
+function Company() {
+    this.employees = [];
+}
+Company.prototype.hireEmployee = function (employee) {
+    this.employees.push(employee);
+}
+Company.prototype.getAllEmployees = function () {
+    return this.employees;
+}
+Company.prototype.getEmployeesBySalary = function (salaryFrom, salaryTo) {
+    const arrEmplBySalary = this.employees.filter(elem => (elem.salary >= salaryFrom && elem.salary <= salaryTo));
+    return arrEmplBySalary.sort((empl, empl1) => empl.salary - empl1.salary);
+}
+
+// Active buttons:
+function addEmployee() {
+    sectEmpl.hidden = true;
+    sectFilterSalary.hidden = true;
+    informPlace.style.display = "flex";
+    OlAllEmployees.hidden = true;
+}
+
+function setAllEmployees() {
+    sectFilterSalary.hidden = true;
+    OlAllEmployees.hidden = false;
+    console.log(company.employees);
+    OlAllEmployees.innerHTML = addAllEmployee(company.employees);
+    informPlace.style.display = "none";
+    sectEmpl.hidden = false;
+}
+function setEmplBySalary() {
+    sectFilterSalary.hidden = false;
+    OLemplBySalary.hidden = false;
+    sectEmpl.hidden = true;
+    OlAllEmployees.hidden = true;
+    informPlace.style.display = "none";
+    outputEmplSalaryFromTo(FROM_SALARY, TO_SALARY);
+}
+
+// function values:
+function addAllEmployee(employees) {
+    const arrEmployees = employees.map(empl =>
+        `<li class="employee">Name: ${empl.employee_name} |Birthdate: ${empl.birthDate} |Department: ${empl.department} |Salary: ${empl.salary} |EMAIL: ${empl.email}</li>`
+    )
+    return arrEmployees.join(' ');
+}
+function outputEmplSalaryFromTo(salaryFrom, salaryTo) {
+    let arrSalary = company.getEmployeesBySalary(salaryFrom, salaryTo);
+    OLemplBySalary.innerHTML = addAllEmployee(arrSalary);
+    informParag.style.color = BLACK;
+    informParag.innerHTML = `List of employees with salary from${FROM_SALARY} to ${TO_SALARY}:`;
+
+}
